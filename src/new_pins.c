7772c9358b3eec2fe0d13f2d1fcd7961422c8f4d
Peter Szoke <ps@jpegs.ro>
2022-09-29 12:57:28 +0200

    fix

--- src/new_pins.c
+++ src/new_pins.c
@@ -568,7 +568,7 @@ static xTaskHandle test_thread = NULL;
 static void timer_handler( beken_thread_arg_t arg )
 {
 	channelTransitionConfig_t *config = (channelTransitionConfig_t*) arg;
-	addLogAdv(LOG_INFO, LOG_FEATURE_GENERAL,"Channel update timer handler called: ch=%i; from=%i; to=%i", &config->ch, &config->from, $config->to);
+	addLogAdv(LOG_INFO, LOG_FEATURE_GENERAL,"Channel update timer handler called: ch=%i; from=%i; to=%i", &config->ch, &config->from, &config->to);
 
 	rtos_delete_thread( NULL );
 	
